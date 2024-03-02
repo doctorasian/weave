2e908fdc962ee8089ee17becc3ed3f3300ac3567
Francis Nguyen <110130404+doctorasian@users.noreply.github.com>
2024-03-02 00:51:47 -0500

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,14 +1,6 @@
-#include <Common.h>
 #include <Engine.h>
-#include <Shader.h>
-#include <cstdlib>
-#include <iostream>
-#include <stb_image.h>
-#include <sys/types.h>
 
 int main() {
-
-  /* TODO: Upgrade Package 3.3 -> 4.x */
   std::cout << "The first step is to start." << std::endl;
   Engine::Run();
   return 0;
